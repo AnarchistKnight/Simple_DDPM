e9bbe4888cf8297da3adf76f5d5e001b4ccf0038
AnarchistKnight <zhengyang219@gmail.com>
2024-11-08 00:29:31 +0800

    revise scripts

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
+python.exe -m pip install --upgrade pip
 pip install git+https://github.com/huggingface/diffusers.git
 pip install transformers
 pip install accelerate
 pip install safetensor
 pip install diffusers
-pip install opencv-python
+pip install opencv-python==4.9.0.80
 pip install IPython
 pip install omegaconf
+pip install visdom
 pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu121
+pip uninstall numpy -y
+pip install numpy==1.26.3
--- train_UNet.sh
+++ train_UNet.sh
@@ -6,8 +6,8 @@ python source/train_unet.py \
 --noise_scale 1.0 \
 --validate_every 10 \
 --save_every 5 \
---train_dataset_directory "D:/playgroundv25/images/train_images_downsample" \
---validate_dataset_directory "D:/playgroundv25/images/test_images_downsample" \
+--train_dataset_directory "images/train_images_downsample" \
+--validate_dataset_directory "images/test_images_downsample" \
 --min_loss_path "min_loss_UNet.json"
 
 
