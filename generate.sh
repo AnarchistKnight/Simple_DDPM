063ae91e7b8bcac35e8d86ffccf96ca4773ef45a
AnarchistKnight <zhengyang219@gmail.com>
2024-11-10 12:01:58 +0800

    reviss

--- generate.sh
+++ generate.sh
@@ -1,5 +1,6 @@
 python source/diffusion_generate.py \
 --generated_image_save_dir generated \
 --n 8 \
+--m 4 \
 --checkpoint checkpoints/latest_model.pth \
 --device cuda
--- source/diffusion_generate.py
+++ source/diffusion_generate.py
@@ -12,7 +12,7 @@ def denormalize_image(img):
 
 
 @torch.no_grad()
-def generate_image(save_dir, n, checkpoint, device):
+def generate_image(save_dir, n, m, checkpoint, device):
     os.makedirs(save_dir, exist_ok=True)
     device = torch.device(device)
     denoiser = build_unet(device)
@@ -23,19 +23,19 @@ def generate_image(save_dir, n, checkpoint, device):
     model.eval()
     image_height = 32
     image_width = 32
-    input = torch.randn(n * n, 3, image_height, image_width).to(device)
+    input = torch.randn(n * m, 3, image_height, image_width).to(device)
     num_steps = 1000
     output = model(input, num_steps)
     output = output.squeeze(0)
     output = torch.permute(output, [0, 2, 3, 1])
     output = output.cpu().numpy()
 
-    big_image = np.zeros((image_height * n, image_height * n, 3), dtype=np.uint8)
-    for i in range(n):
+    big_image = np.zeros((image_height * m, image_width * n, 3), dtype=np.uint8)
+    for i in range(m):
         for j in range(n):
             generated_image = denormalize_image(output[i * n + j])
             big_image[i * image_height:(i + 1) * image_height, j * image_width:(j + 1) * image_width] = generated_image
-    save_path = os.path.join(save_dir, f"generated_images_{n}x{n}.png")
+    save_path = os.path.join(save_dir, f"generated_images_{n}x{m}.png")
     cv2.imwrite(save_path, big_image)
 
 
@@ -43,11 +43,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="input arguments for diffusion generate")
     parser.add_argument("--generated_image_save_dir", type=str, default="images/generated")
     parser.add_argument("--n", type=int, default=8)
+    parser.add_argument("--m", type=int, default=4)
     parser.add_argument("--checkpoint", type=str, default="checkpoints/latest_model.pth")
     parser.add_argument("--device", type=str, default="cuda")
     args = parser.parse_args()
     generate_image(args.generated_image_save_dir,
                    args.n,
+                   args.m,
                    args.checkpoint,
                    args.device)
 
